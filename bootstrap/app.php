ff2cf858798134e29835285c2308b8404e279049
fatima-zahra01 <elmagana.fatima-zahra@ensam-casa.ma>
2026-04-18 13:55:26 +0100

    feat: add role-based middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,8 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'isAdmin'     => \App\Http\Middleware\IsAdmin::class,
+            'isRecruteur' => \App\Http\Middleware\IsRecruteur::class,
+            'isCandidat'  => \App\Http\Middleware\IsCandidat::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
