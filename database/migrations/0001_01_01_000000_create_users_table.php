2baeb5dc3d57aba3b89810c769db66640cf5c3e1
fatima-zahra01 <elmagana.fatima-zahra@ensam-casa.ma>
2026-04-18 13:27:46 +0100

    feat: add migrations and eloquent models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['candidat', 'recruteur', 'admin'])->default('candidat');
             $table->rememberToken();
             $table->timestamps();
         });
